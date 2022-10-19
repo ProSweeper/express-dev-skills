d00184c920d1caa95edaec1973ba36e63869b4af
ProSweeper <dylan.doucet28@gmail.com>
2022-10-19 18:55:35 +0000

    fix bugs and path errors

--- controllers/skills.js
+++ controllers/skills.js
@@ -9,7 +9,7 @@ module.exports = {
 function index(req, res) {
     // for our render functions we configured the app to start in the views folder
     // thats why we dont pass that into the path param
-    res.render('/skills/index', {
+    res.render('skills/index', {
         // the render function has 2 params the path and an object
         // the value of the object we want to render is recieved from our model
         // function we imported into this module
@@ -20,7 +20,8 @@ function index(req, res) {
 // respond to request asking for a single entry
 function show(req, res) {
     // remember we are rendering the view ejs file with this function
-    res.render('/skills/show', {
+    // never use leading slashes when rendering
+    res.render('skills/show', {
         // the request object has a parameters property which we can get the id from
         // we call this one skill because we are only retunring a singular entry
         skill: Skill.getOne(req.params.id)
